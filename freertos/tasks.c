8c9d3b2e38d261d4e83c20784e7d47297991f0df
xiaoming <2014500726@smail.xtu.edu.cn>
2022-01-14 12:11:19 +0800

    fix: warring
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- freertos/tasks.c
+++ freertos/tasks.c
@@ -39,6 +39,7 @@ task.h is included from an application file. */
 #include "task.h"
 #include "timers.h"
 #include "stack_macros.h"
+#include "UARTCommandConsole.h"
 
 /* Lint e9021, e961 and e750 are suppressed as a MISRA exception justified
 because the MPU ports require MPU_WRAPPERS_INCLUDED_FROM_API_FILE to be defined
